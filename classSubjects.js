0fb2b7762575632ced06a468573525a25ff0a06a
johnseian24 <166204258+johnseian24@users.noreply.github.com>
2024-10-30 21:51:27 +0800

    Create classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,11 @@
+// User Information Collection for Class Subject
+
+// Variables to store user information
+let SubjectTitle = "Mathematics";
+let classSchedule = "11:00 AM - 12:00";
+let classRoom = "Room 102";
+let classInstructor = "Mr.Owen";
+let studentName = "John Seian";
+
+// log user information to the console in the Specified format 
+console.log(`${studentName} is currently enrolled in ${SubjectTitle} With a class schedule of ${classSchedule} at ${classRoom}. the intructor is ${classInstructor}.`);
