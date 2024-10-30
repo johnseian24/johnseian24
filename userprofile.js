6ebf4262a5b9077a4d31a2ed5b384d11e00987fc
johnseian24 <166204258+johnseian24@users.noreply.github.com>
2024-10-30 22:58:16 +0800

    userprofile.js

---
+++ userprofile.js
@@ -0,0 +1,12 @@
+// User Profile Object
+
+// Create the userProfile object with user's details
+let userProfile = {
+fullName: "john seian",        
+age: 28,                         
+favoriteNumber: 7,               
+favoriteColors: ["black", "white", "red"] // Replace with actual user input or promptm
+};
+
+// Log the entire userProfile object to the console
+console.log("User Profile:", userProfile);
